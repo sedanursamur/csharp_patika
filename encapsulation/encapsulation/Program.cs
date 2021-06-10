a34be833b4582ee4388aa535c1d06f80f7e01e43
Sedanur SAMUR <81965223+sedanursamur@users.noreply.github.com>
2021-06-10 21:31:27 +0300

    Add files via upload

---
+++ encapsulation/encapsulation/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci("Sedanur", "Samur", 396, 4);
+            ogrenci1.OgrenciBilgileri();
+            Console.WriteLine("***********************");
+            ogrenci1.SinifAtlat();
+            ogrenci1.OgrenciBilgileri();
+        }
+    }
+    class Ogrenci
+    {
+        private string isim;
+        private string soyisim;
+        private int ogrenciNo;
+        private int sinif;
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+        public int OgrenciNo { get => ogrenciNo; set => ogrenciNo = value; }
+        public int Sinif
+        {
+            get => sinif;
+            set
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("Sınıf en az 1 olabiliri");
+                    sinif = 1;
+                }
+                else
+                    sinif = value;
+            }
+        }
+
+        public Ogrenci() { }
+
+        public Ogrenci(string isim, string soyisim, int ogrenciNo, int sinif)
+        {
+            this.Isim = isim;
+            this.Soyisim = soyisim;
+            this.OgrenciNo = ogrenciNo;
+            this.Sinif = sinif;
+        }
+
+        public void OgrenciBilgileri()
+        {
+            Console.WriteLine("Öğrenci adı :          {0}", this.Isim);
+            Console.WriteLine("Öğrenci soyadı :       {0}", this.Soyisim);
+            Console.WriteLine("Öğrenci nosu :         {0}", this.OgrenciNo);
+            Console.WriteLine("Öğrenci sınıfı :       {0}", this.Sinif);
+
+        }
+
+        public void SinifAtlat()
+        {
+            this.Sinif = this.Sinif + 1;
+        }
+
+        public void SinifDusur()
+        {
+            this.Sinif = this.Sinif - 1;
+        }
+
+    }
+}
---
+++ enum/enum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _enum
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        // sıralı veriler 
+        Console.WriteLine(Gunler.Pazar);
+        Console.WriteLine((int)Gunler.CUmartesi);//numeric değerine ulaştık
+
+        int sicaklik = 24;
+
+        if (sicaklik <= (int)HavaDurumu.Normal)//sicaklık normalin altındaysa
+        {
+            Console.WriteLine("Dışarıya çıkmak için havanın ısınmasını bekle");
+        }
+
+        else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSıcak)
+        {
+            Console.WriteLine("Hadi dışarıya çıkalım");
+        }
+        else if (sicaklik >= (int)HavaDurumu.Normal)
+        {
+            Console.WriteLine("Dışarıya çıkmak için sıcak bir gün");
+        }
+
+    }
+    enum Gunler //sabit değerleri tutmak için burada tutabiliriz
+    {
+        Pazartesi = 1, //elemanımın adı
+        Sali,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        CUmartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
+}
