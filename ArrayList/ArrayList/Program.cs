0b72041addad74a33f165be35b1c374b7436c07a
Sedanur SAMUR <81965223+sedanursamur@users.noreply.github.com>
2021-06-10 21:27:55 +0300

    Add files via upload

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("Ayşe");
+            list.Add(2);
+            list.Add(true);
+            list.Add('s');
+
+
+            //içerisindeki verilere erişim :
+            Console.WriteLine(list[1]);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //AddRange -> birden fazla elemanı toplu halde nasıl ekleriz ?
+            string[] renkler = { "kırmızı", "mavi" };
+            List<int> sayilar = new List<int>() { 1, 5, 3, 6, 4, 4 };
+            list.AddRange(renkler);//dizi ekledik
+            list.AddRange(sayilar);//generic ekledik
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort -> sayilara göre sıralar
+            //list.Sort();
+
+            //BinarySearch -> sıralama yapıldıktan sonra doğru çalışıyor 
+
+            //Console.WriteLine(list.BinarySearch(3));
+
+            //Reverse tersini sıralar
+
+            //clear -Z listeyi temizler
+
+
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan("Eren", "Yılmaz", 312312, "Yazılım");
+
+
+
+            Calisan calisan2 = new Calisan();
+            calisan1.Ad = "Deniz";
+            calisan1.Soyad = "Arda";
+            calisan1.No = 32131231;
+            calisan1.Departman = "Pazarlama";
+
+            calisan1.CalisanBilgileri();
+            calisan2.CalisanBilgileri();
+
+            Console.WriteLine(Islemler.Topla(50, 100));
+            Console.WriteLine(Islemler.Cikar(350, 100));
+        }
+    }
+
+
+    class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int No { get; set; }
+        public string Departman { get; set; }
+
+
+        public Calisan()
+        {
+
+        }
+
+
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+
+
+
+        public Calisan(string ad, string soyad, int no, string departman) : this(ad, soyad)
+        {
+            this.No = no;
+            this.Departman = departman;
+        }
+
+
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine($"Çalışan Adı : {Ad}");
+            Console.WriteLine($"Çalışan Soyadı : {Soyad}");
+            Console.WriteLine($"Çalışan Numarası : {No}");
+            Console.WriteLine($"Çalışan Departmanı : {Departman}");
+        }
+    }
+
+
+
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+    
+}
