006a491ce9c1f0a5ede829ecb0b9f7fd9d71763d
Sedanur SAMUR <81965223+sedanursamur@users.noreply.github.com>
2021-06-10 21:33:20 +0300

    Add files via upload

---
+++ Error_Management/Error_Management/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Error_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş Olduğunuz Sayı : " + sayi);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata : " + e.Message.ToString());
+            }
+
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı.");
+            }
+
+
+
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("test");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz.");
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri Tipi Uygun Değil.");
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı");
+            }
+
+            try
+            {
+                int a = int.Parse("-20000000000");
+
+            }
+            catch (ArgumentNullException ex)
+            {
+
+                Console.WriteLine("Boş değer girdiniz...");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("veri tipi uygun değil");
+                Console.WriteLine(ex);
+
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("çok küçük yada çok büyük değer girdiniz...");
+                Console.WriteLine(ex);
+
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı...");
+            
+            }
+        }
+    }
+}
---
+++ erisim_belirleyicileri/erisim_belirleyicileri/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace erisim_belirleyicileri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan("Sedanur", "Soyad", 396, "İnsan kaynakları");
+
+            calisan1.CalisanBilgileri();
+            Console.WriteLine("*******************");
+
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Sedanur";
+            calisan2.Soyad = "Soyad";
+            calisan2.No = 396;
+            calisan2.Departman = "İnsan kaynakları";
+
+            calisan2.CalisanBilgileri();
+
+            Console.WriteLine("*******************");
+            Calisan calisan3 = new Calisan("Sedanur", "Soyad");
+
+            calisan3.CalisanBilgileri();
+        }
+    }
+
+
+
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+
+        public int No;
+
+        public string Departman;
+
+        public Calisan(string ad, string soyad, int no, string departman)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+        }
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+
+        }
+
+        public Calisan()
+        {
+
+        }
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı:{0}", Ad);
+            Console.WriteLine("Çalışan Soyadı:{0}", Soyad);
+            Console.WriteLine("Çalışan No:{0}", No);
+            Console.WriteLine("Çalışan Departman:{0}", Departman);
+        }
+
+    }
+
+}
