89919c5cafaa7ec5a7dfb80b7e8a813b4d32c028
Sedanur SAMUR <81965223+sedanursamur@users.noreply.github.com>
2021-06-10 21:40:15 +0300

    Add files via upload

---
+++ static_sinif/static_sinif/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace static_sinif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //static değişkenlere , metotlara nesne oluşturmadan erişiriz ve  kalıtım işlemi uygulanamaz
+            // static olan elemanlara o sınıfın adıyla erişebiliriz
+            // static olmayan özelliklere nesne aracılığyla erişiyoruz    
+            Console.WriteLine("Çalışan sayısı: {0}", Calisan.CalisanSayisi);
+            //CalisanSayisi değişkeni artık bir değişken gibi davranır
+
+
+            Calisan calisan1 = new Calisan("Sedanur", "Samur", "IK");
+            //calisan1. dediğimizde özelliklere erişim sağlayamayız -> 'private'
+            Console.WriteLine("Çalışan sayısı: {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan2 = new Calisan("Sedanur", "Samur", "IK");
+            Console.WriteLine("Çalışan sayısı: {0}", Calisan.CalisanSayisi);
+
+
+            Console.WriteLine("Toplama  :{0}", Islemler.Topla(15, 9));
+            Console.WriteLine("Çıkartma :{0}", Islemler.Cikar(15, 9));
+        }
+    }
+    class Calisan
+    {
+        private static int calisanSayisi; //static değişken  -> calisanSayisi sınıfa ait bir özellik gibi düşünülebilir
+
+        public static int CalisanSayisi { get => calisanSayisi; } //encapsulation  -> set yapmadık çünkü sadece calisan classından değiştirmek istiyoruz.
+
+        //static kurucu :
+        static Calisan()
+        {   //erişim belirteçleri olmaz
+            calisanSayisi = 0;//olulturduğumda değeri 0 olarak atanıyor 
+
+        }
+        private string Isim;
+        private string SoyIsim;
+        private string Departman;
+
+        public Calisan(string isim, string soyIsim, string departman)//cons.
+        {
+            this.Isim = isim;
+            this.SoyIsim = soyIsim;
+            this.Departman = departman;
+            calisanSayisi++; // her nesne oluştuğunda calisanSayisi 1 artacak
+        }
+    }
+
+    static class Islemler
+    {//static class içinde tüm elemanlar static olur
+
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+
+    }
+}
---
+++ ödev_2/ödev_2/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+
+
+namespace ödev_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //question 1:
+
+            ArrayList asalSayilar = new ArrayList();
+            ArrayList asalOlmayanSayilar = new ArrayList();
+
+            Console.WriteLine("20 tane sayı girişi yapınız");
+
+
+
+            try
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    int alinanSayi;
+                    alinanSayi = Int32.Parse(Console.ReadLine());
+                    if (alinanSayi < 2)
+                    {
+                        Console.WriteLine("Küçük bir sayı girdiniz.");
+                        continue;
+                    }
+
+
+                    if (alinanSayi == 2 || alinanSayi == 3)
+                    {
+                        asalSayilar.Add(alinanSayi);
+                    }
+
+                    else if (alinanSayi % 2 == 0 || alinanSayi % 3 == 0)
+                    {
+                        asalOlmayanSayilar.Add(alinanSayi);
+                    }
+
+                    else
+                    {
+                        asalSayilar.Add(alinanSayi);
+                    }
+
+                }
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("Veri tipi uygun verilmedi...");
+                Console.WriteLine(ex);
+            }
+
+            finally
+            {
+
+
+
+
+                asalOlmayanSayilar.Sort();
+                asalSayilar.Sort();
+
+                Console.WriteLine("Asal olmayan sayilar :");
+                foreach (var item in asalOlmayanSayilar)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("Asal sayilar :");
+                foreach (var item in asalSayilar)
+                {
+                    Console.WriteLine(item);
+                }
+
+                int asalToplam = 0;
+                int asalOlmayanToplam = 0;
+                foreach (var item in asalSayilar)
+                {
+                    asalToplam += int.Parse(item.ToString());
+                }
+                foreach (var item in asalOlmayanSayilar)
+                {
+                    asalOlmayanToplam += int.Parse(item.ToString());
+                }
+
+                Console.WriteLine
+                (
+                "Asal olmayan sayı dizisinin elaman sayısı : " + asalOlmayanSayilar.Count + "\n" +
+                "Asal olmayan sayı dizisinin elemanlarının ortalaması : " + asalOlmayanToplam + "\n" + "Asal olan sayı dizisinin eleman sayısı : " + asalSayilar.Count + "\n" +
+                 "Asal sayı dizisinin elemanlarının ortalaması :" + asalToplam
+                );
+
+            }
+
+
+
+            //question 2:
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("20 sayı girişi yapınız :");
+            ArrayList newArray = new ArrayList();
+            int alinanSayi1;
+            for (int i = 0; i < 20; i++)
+            {
+                alinanSayi1 = Int32.Parse(Console.ReadLine());
+                newArray.Add(alinanSayi1);
+            }
+
+
+
+            newArray.Sort();
+
+            ArrayList enBuyuk = new ArrayList();
+            int[] enKucuk = new int[3];
+
+            for (int i = newArray.Count - 3; i <= newArray.Count - 1; i++)
+            {
+                enBuyuk.Add(int.Parse(newArray[i].ToString()));
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                enKucuk[i] = int.Parse(newArray[i].ToString());
+            }
+
+
+            int enBuyukToplam = 0;
+            int enKucukToplam = 0;
+
+            foreach (var item in enBuyuk)
+            {
+                enBuyukToplam += int.Parse(item.ToString());
+            }
+            foreach (var item in enKucuk)
+            {
+                enKucukToplam += int.Parse(item.ToString());
+            }
+
+            Console.WriteLine("3 büyük sayının ortalamalası : " + enBuyukToplam / 3 + "\n3 küçük sayının ortalaması : " + enKucukToplam / 3 + "\nGirilen 20 sayının en büyük 3 sayının ve en küçük 3 sayının  ortalamalarının toplamı : " + ((enBuyukToplam / 3) + (enKucukToplam / 3)));
+
+
+            //question 3:
+
+            Console.WriteLine("--------------");
+            Console.WriteLine("Bir cümle giriniz..");
+            string metin = Console.ReadLine();
+            string sesli = "aeıioöuü";
+
+            int sayac = 0;
+            for (int i = 0; i < metin.Length; i++)
+            {
+            
+                if (sesli.Contains(metin[i]))
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine(metin);
+            Console.WriteLine("Yukarıdaki ifadede toplam {0} tane sesli harf vardır.", sayac);
+
+        }
+    }
+}
